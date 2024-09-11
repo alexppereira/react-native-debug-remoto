cf81e95e214f4ed2935e7b42c38e8a5fca67d5df
alexppereira <suporte@alexpereira.net.br>
2024-09-11 08:32:56 -0300

    chore: ajusta postinstall

--- patches/aplicar-patches.sh
+++ patches/aplicar-patches.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-echo "Diretório atual: $(pwd)"
 
-patch --forward -i ./patches/react-native+0.73.6-log-box-inspector.patch "react-native/Libraries/LogBox/UI/LogBoxInspector.js"
-patch --forward -i ./patches/react-native+0.73.6-modal.patch "react-native/Libraries/Modal/Modal.js"
+# Use a variável de ambiente INIT_CWD para navegar ao diretório raiz do projeto pai
+PROJECT_ROOT=${INIT_CWD}
+
+# Exibe o diretório do projeto pai
+echo "Diretório do projeto pai: $PROJECT_ROOT"
+
+# Aplica os patches no react-native do projeto pai
+patch --forward -i "$PROJECT_ROOT/patches/react-native+0.73.6-log-box-inspector.patch" "$PROJECT_ROOT/node_modules/react-native/Libraries/LogBox/UI/LogBoxInspector.js"
+patch --forward -i "$PROJECT_ROOT/patches/react-native+0.73.6-modal.patch" "$PROJECT_ROOT/node_modules/react-native/Libraries/Modal/Modal.js"
