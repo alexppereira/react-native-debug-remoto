f0a62972517cb910415adbd316cc06228a81ff78
alexppereira <suporte@alexpereira.net.br>
2024-09-11 08:26:56 -0300

    chore: ajusta postinstall

--- patches/aplicar-patches.sh
+++ patches/aplicar-patches.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "Diretório atual: $(pwd)"
 
-patch --forward -i ./patches/react-native+0.73.6-log-box-inspector.patch "../react-native/Libraries/LogBox/UI/LogBoxInspector.js"
-patch --forward -i ./patches/react-native+0.73.6-modal.patch "../react-native/Libraries/Modal/Modal.js"
+patch --forward -i ./patches/react-native+0.73.6-log-box-inspector.patch "../../react-native/Libraries/LogBox/UI/LogBoxInspector.js"
+patch --forward -i ./patches/react-native+0.73.6-modal.patch "../../react-native/Libraries/Modal/Modal.js"
